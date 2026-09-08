b30fc32bbb6e26675a64b5e59030b1f351bb353d
Matias Soto <matias.soto.zuniga@gmail.com>
2026-09-08 02:22:35 -0300

    fix: product list size

--- back/catalogue/views.py
+++ back/catalogue/views.py
@@ -9,7 +9,7 @@ from .serializer import ProductSerializer, CategorySerializer
 class ProductPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 100
+    max_page_size = 50
 
 
 class CategoryListView(generics.ListAPIView):

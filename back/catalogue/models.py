487dff4532f9c34a0e73142321de47003eeeb8ea
Matias Soto <matias.soto.zuniga@gmail.com>
2026-09-08 02:52:19 -0300

    fix: database correction and requirements

--- back/catalogue/models.py
+++ back/catalogue/models.py
@@ -1,18 +1,30 @@
 from django.db import models
+from decimal import Decimal
+from django.core.validators import MinValueValidator
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=3)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=3,
+        validators=[
+            MinValueValidator(Decimal("0.0"))
+        ])
     stock = models.PositiveIntegerField()
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["name"])
+        ]
 
     def __str__(self):
         return self.name
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     class Meta:
         verbose_name = "Category"

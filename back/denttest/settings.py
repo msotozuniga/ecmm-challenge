1f7080bd028814f9686c238bcd847e5a76280b92
Matias Soto <matias.soto.zuniga@gmail.com>
2026-09-09 19:38:36 -0300

    fix: environment variables

--- back/denttest/settings.py
+++ back/denttest/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,5 +134,5 @@ MAILERS = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    os.getenv("FRONTEND_URL"),
 ]

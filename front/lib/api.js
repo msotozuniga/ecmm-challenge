3902bb3e463fdfc3767e6dab82bda3b65571fb12
Matias Soto <matias.soto.zuniga@gmail.com>
2026-09-09 22:29:44 -0300

    fix: env and setting fixes

--- front/lib/api.js
+++ front/lib/api.js
@@ -1,4 +1,4 @@
-const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "http://127.0.0.1:8000";
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 async function request(path, options = {}) {
   const response = await fetch(`${backendUrl}/api${path}`, {
